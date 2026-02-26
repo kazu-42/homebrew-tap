56d714d75c8be4238c0a180d1465db83ddacad03
kazu-42 <91968271+kazu-42@users.noreply.github.com>
2026-02-26 20:54:14 +0900

    Update SHA256 for v1.1.0

--- Casks/mdpreview.rb
+++ Casks/mdpreview.rb
@@ -1,8 +1,8 @@
 cask "mdpreview" do
   version "1.1.0"
-  sha256 "TODO: Update with actual SHA256 after release"
+  sha256 "493167343390b0ff810fdf68bfcacc0e8cad5d65f7af71436ddad79f51b3950f"
 
-  url "https://github.com/kazu-42/mdpreview/releases/download/v#{version}/MDPreview-#{version}.dmg"
+  url "https://github.com/kazu-42/mdpreview/releases/download/v#{version}/MDPreview.dmg"
   name "MDPreview"
   desc "Lightweight Markdown preview app for macOS"
   homepage "https://github.com/kazu-42/mdpreview"
