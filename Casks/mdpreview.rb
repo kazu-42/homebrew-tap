76003b033f284525a3a63c0cd0e0c734afecd62e
kazu-42 <91968271+kazu-42@users.noreply.github.com>
2026-02-27 00:55:42 +0900

    fix: Correct sha256 checksum

--- Casks/mdpreview.rb
+++ Casks/mdpreview.rb
@@ -1,6 +1,6 @@
 cask "mdpreview" do
   version "1.2.3"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "c625a13d6094075d51ddfba94b2899864ab22a2fc8148db243140e634ac529de"
 
   url "https://github.com/kazu-42/mdpreview/releases/download/v#{version}/MDPreview-#{version}.dmg"
   name "MDPreview"
