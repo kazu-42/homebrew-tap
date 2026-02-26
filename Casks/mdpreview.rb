04fa1b233b7485954f0763ec4015ddd708b23e61
kazu-42 <91968271+kazu-42@users.noreply.github.com>
2026-02-27 00:54:39 +0900

    fix: Use versioned DMG filename

--- Casks/mdpreview.rb
+++ Casks/mdpreview.rb
@@ -2,7 +2,7 @@ cask "mdpreview" do
   version "1.2.3"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/kazu-42/mdpreview/releases/download/v#{version}/MDPreview.dmg"
+  url "https://github.com/kazu-42/mdpreview/releases/download/v#{version}/MDPreview-#{version}.dmg"
   name "MDPreview"
   desc "Lightweight Markdown preview app for macOS"
   homepage "https://github.com/kazu-42/mdpreview"
